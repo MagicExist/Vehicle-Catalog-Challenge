e7671a90675e43f53debfdc070886531ce1ddbbb
MagicExist <johhanparra1@gmail.com>
2025-03-29 19:20:04 -0500

    feat(initial-create):add initial setup configuration project

---
+++ database/migrations/2025_03_29_232802_create_cars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id('car_id');
+            $table->string('model',30);
+            $table->string('description',100);
+            $table->decimal('price',10,2);
+            $table->decimal('mileage',10,2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
